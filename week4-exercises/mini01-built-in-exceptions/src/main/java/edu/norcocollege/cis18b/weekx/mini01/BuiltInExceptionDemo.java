b5e7abf15210b2e7f6ab042c3edc85ecca8945db
llacidrainll <gdgarcia1209@gmail.com>
2026-03-22 17:23:18 +0000

    fixed few errors

--- week4-exercises/mini01-built-in-exceptions/src/main/java/edu/norcocollege/cis18b/weekx/mini01/BuiltInExceptionDemo.java
+++ week4-exercises/mini01-built-in-exceptions/src/main/java/edu/norcocollege/cis18b/weekx/mini01/BuiltInExceptionDemo.java
@@ -5,9 +5,20 @@ public class BuiltInExceptionDemo {
         int numerator = 10;
         int denominator = 0;
         String[] alerts = {"CPU High", "Disk Full", "Login Failure"};
-
+        
         // TODO: Use try/catch to handle division by zero.
         // Expected friendly message example: Cannot divide by zero.
+        try {int result = numerator/denominator;
+            System.out.println("Result: " + result)
+        }
+        catch (ArithmeticException ex)
+        {
+            System.out.println("Cannot divide by zero.")
+        }
+        
+        try {
+            
+        }
 
         // TODO: Use try/catch to handle an invalid alert index.
         // Purposely try an out-of-range index so the catch block runs.
